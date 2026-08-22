38e11bbc39db78459e3758024d27a48c0da3096b
mohdabd93 <57831136+mohdabd93@users.noreply.github.com>
2026-08-22 20:22:13 +0300

    Update capacitor.config.ts

--- my-app/capacitor.config.ts
+++ my-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'life-organizer',
-  webDir: 'dist'    
+  webDir: 'build' 
 };
 
 export default config;
